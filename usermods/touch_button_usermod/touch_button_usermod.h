0a380ef8f8abc8cbff53041dd3e4e73b6938eda1
madhukar.temba <madhukar.temba@arcadia.com>
2025-01-19 22:38:02 +0530

    add reduce and increase brightness buttons

--- usermods/touch_button_usermod/touch_button_usermod.h
+++ usermods/touch_button_usermod/touch_button_usermod.h
@@ -12,7 +12,6 @@ private:
     std::function<void()> onClick;
 
 public:
-
     Button(int pin)
         : pin(pin) {};
 
@@ -37,17 +36,63 @@ public:
 class TouchButtonUsermod : public Usermod
 {
 private:
-    Button button = Button(27);
+    Button onOffButton = Button(27);
+    Button reduceBrightnessButton = Button(26);
+    Button increaseBrightnessButton = Button(25);
+
+public:
+    void toggleStrip()
+    {
+        if (bri > 0)
+        {
+            briLast = bri;
+            bri = 0;
+        }
+        else
+        {
+            bri = briLast > 0 ? briLast : 255;
+        }
+        stateUpdated(CALL_MODE_BUTTON);
+        strip.show();
+    }
+
+    void reduceBrightness()
+    {
+        if (bri > 0)
+        {
+            bri = max(bri - 10, 0);
+            stateUpdated(CALL_MODE_BUTTON);
+            strip.show();
+        }
+    }
+
+    void increaseBrightness()
+    {
+        if (bri < 255)
+        {
+            bri = min(bri + 10, 255);
+            stateUpdated(CALL_MODE_BUTTON);
+            strip.show();
+        }
+    }
 
 public:
     void setup()
     {
-        button.setup([]()
-                           { Serial.println("Button clicked!"); });
+        onOffButton.setup([this]()
+                          { toggleStrip(); });
+
+        reduceBrightnessButton.setup([this]()
+                                     { reduceBrightness(); });
+
+        increaseBrightnessButton.setup([this]()
+                                       { increaseBrightness(); });
     }
 
     void loop()
     {
-        button.run();
+        onOffButton.run();
+        reduceBrightnessButton.run();
+        increaseBrightnessButton.run();
     }
 };
